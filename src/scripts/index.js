b9209f63ca9b4a8a4195d88e007b50b4ba475fa6
Huynh Duc <huynhduc.cntt@gmail.com>
2018-08-08 15:16:08 +0700

    struct
    Build Struct

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Router from "react-router-dom/BrowserRouter";
+// import Route from "react-router-dom/Route";
+
+
+// const Transition = () => (
+//     <h1> hey</h1>
+// );
+// ReactDOM.render(<Transition />, document.getElementById("root"));
